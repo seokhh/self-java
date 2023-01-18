ea8a13849c2656641dd6fdd977c1118b902603cf
seokhh <119908334+seokhh@users.noreply.github.com>
2023-01-18 20:49:24 +0900

    Add files via upload

---
+++ 혼공자 p.52 직접해보는 손코딩.java
@@ -0,0 +1,11 @@
+package sec01.exam01;
+
+public class Main {
+    public static void main(String[] args){
+        int value = 30;
+
+        int result = value + 10;
+
+        System.out.println(result);
+    }
+}
