73fffa96bda99825d257867c35d04ad0594135d3
seokhh <119908334+seokhh@users.noreply.github.com>
2023-01-19 20:03:32 +0900

    Add files via upload

---
+++ 혼공자 p.68 직접해보는 손코딩.java
@@ -0,0 +1,23 @@
+public class Main {
+    public static void main(String[] args){
+
+        float a = 3.14f; //float타입은 끝에 f를 붙여야함
+        double b = 3.14;
+
+        float c = 0.12345678901234456789f;
+        double d = 0.12345678901234456789f;
+
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+        System.out.println(d);
+
+        double var1 = 3e6;
+        float var2 = 3e6f;
+        double var3 = 2e-3;
+
+        System.out.println(var1);
+        System.out.println(var2);
+        System.out.println(var3);
+    }
+}
