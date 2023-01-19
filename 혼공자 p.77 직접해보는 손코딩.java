e095030d0384034fb37f4d44c96735e4bd9530db
seokhh <119908334+seokhh@users.noreply.github.com>
2023-01-19 20:30:24 +0900

    Add files via upload

---
+++ 혼공자 p.77 직접해보는 손코딩.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args){
+
+        byte result1 = 10+20;
+        System.out.println(result1);
+
+        byte x = 10;
+        byte y = 20;
+        int result2 = x+y;
+        System.out.println(result2);
+        }
+    }
+
---
+++ 혼공자 p.78 직접해보는 손코딩.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args){
+
+        byte a = 10;
+        int b = 100;
+        long c = 1000L;
+
+        long result = a + b + c; //가장 큰 타입이 long이기때문에 long으로 결고값을 만듦.
+        System.out.println("a+b+c = " + result);
+        }
+    }
+
