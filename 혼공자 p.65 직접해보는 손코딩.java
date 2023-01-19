66bc220b723402e0a7db2700b258a69cfd94a009
seokhh <119908334+seokhh@users.noreply.github.com>
2023-01-19 19:56:06 +0900

    Add files via upload

---
+++ 혼공자 p.65 직접해보는 손코딩.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args){
+        String name = "석해현";
+        String job = "학생";
+
+        System.out.println(name);
+        System.out.println(job);
+    }
+}
