3865c9b662013baf7b11679e6f185798be75a4da
seokhh <119908334+seokhh@users.noreply.github.com>
2023-01-19 19:59:30 +0900

    Add files via upload

---
+++ 혼공자 p.66 직접해보는 손코딩.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args){
+        System.out.println("번호\t이름\t나이");
+        System.out.print("줄 바꿈 \n");
+        System.out.println("우리는 \"개발자\" 입니다.");
+        System.out.println("봄\\여름\\가을\\겨울");
+    }
+}
