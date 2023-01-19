1d9549ee296900f314eb50ec184ab87e3612b334
seokhh <119908334+seokhh@users.noreply.github.com>
2023-01-19 20:06:19 +0900

    Add files via upload

---
+++ 혼공자 p.69 직접해보는 손코딩.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args){
+
+        boolean stop = false; // true 입력시 중지합니다라는 문구가 출력됨
+        if(stop){
+            System.out.println("중지합니다.");
+        }
+        else{
+            System.out.println("시작합니다.");
+        }
+    }
+}
