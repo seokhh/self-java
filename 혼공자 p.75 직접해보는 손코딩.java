bbb9b81b6b6cf850f664c9a5f55f2b99ba87dbb0
seokhh <119908334+seokhh@users.noreply.github.com>
2023-01-19 20:25:33 +0900

    Add files via upload

---
+++ 혼공자 p.75 직접해보는 손코딩.java
@@ -0,0 +1,18 @@
+public class Main {
+    public static void main(String[] args){
+
+        int intValue = 44032;
+        char charValue = (char) intValue;
+        System.out.println("charValue: " + charValue);
+
+        long longValue = 500;
+        intValue = (int) longValue;
+        System.out.println("intValue: " + intValue);
+
+
+        double doubleValue = 3.14;
+        intValue =(int) doubleValue;
+        System.out.println("intValue: " + intValue);
+        }
+    }
+
