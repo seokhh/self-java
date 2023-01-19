0a25dc1417ca1070605d6828664f30e3ac1580ce
seokhh <119908334+seokhh@users.noreply.github.com>
2023-01-19 19:54:05 +0900

    Add files via upload

---
+++ 혼공자 p.64 직접해보는 손코딩.java
@@ -0,0 +1,17 @@
+public class Main {
+    public static void main(String[] args){
+        char c1 = 'A';
+        char c2 = 65;
+        char c3 = '\u0041';
+        char c4 = '가';
+        char c5 = 44032;
+        char c6 = '\uac00';
+
+        System.out.println(c1);
+        System.out.println(c2);
+        System.out.println(c3);
+        System.out.println(c4);
+        System.out.println(c5);
+        System.out.println(c6);
+    }
+}
