43575d658300807bb09a3f0f6623793749a84106
seokhh <119908334+seokhh@users.noreply.github.com>
2023-01-20 21:26:24 +0900

    Add files via upload

---
+++ 혼공자 p.87 마무리 문제 5번.java
@@ -0,0 +1,10 @@
+/* 문제
+    a의 유니코드는 97이다 b의 유니코드는 98이다 a에서 1을 더하는 방식으로 b를 출력하시오*/
+public class Main {
+    public static void main(String[] args){
+        char c1 = 'a';
+        char c2 = (char) (c1 + 1);
+        System.out.println(c2);
+        }
+    }
+
