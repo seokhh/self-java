c22cc0ec998b2b4f8e529fef40d141ca537bc740
seokhh <119908334+seokhh@users.noreply.github.com>
2023-01-20 21:16:29 +0900

    다음 코드에서 컴파일 에러가 발생하는 구간과 이유를 적으시오

---
+++ 혼공자 p.86 마무리 문제 4번.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args){
+        short s1 = 1;
+        short s2 = 2;
+        int i1= 3;
+        int i2 = 4;
+        /* short result1 = s1 + s2; 연산결과가 int 타입이기 때문에 오류가 발생*/
+        int result2 = i1 + i2;
+        }
+    }
+
