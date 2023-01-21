eb2ef7a6a2b92e6a79804aaf3362314642db8c85
seokhh <119908334+seokhh@users.noreply.github.com>
2023-01-21 20:35:17 +0900

    Add files via upload

---
+++ 혼공자 p.81 직접해보는 손코딩.java
@@ -0,0 +1,29 @@
+public class Main {
+    public static void main(String[] args){
+        byte byteValue1 = 10;
+        byte byteValue2 = 20;
+
+        int intValue1 = byteValue1 + byteValue2;
+        System.out.println(intValue1);
+
+        char charValue1 = 'A';
+        char charValue2 = 1;
+        int intValue2 = charValue1+charValue2;
+        System.out.println("유니코드= " + intValue2);
+        System.out.println("출력문자= " + (char)intValue2);
+
+        int intValue3 = 10;
+        int intValue4 = intValue3/4;
+        System.out.println(intValue4);
+
+        int intValue5 = 10;
+        double doubleValue = intValue5 / 4.0;
+        System.out.println(doubleValue);
+
+        int x = 1;
+        int y = 2;
+        double result = (double) x/y;
+        System.out.println(result);
+    }
+}
+
