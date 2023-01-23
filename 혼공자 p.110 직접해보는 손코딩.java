f5fc1def1f8cd31d5623c6ec71dfd92bcf9d0afd
seokhh <119908334+seokhh@users.noreply.github.com>
2023-01-23 20:25:07 +0900

    Add files via upload

---
+++ 혼공자 p.110 직접해보는 손코딩.java
@@ -0,0 +1,20 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+
+        int x = -100;
+        int result1 = +x;
+        int result2 = -x;
+        System.out.println("result1= " + result1);
+        System.out.println("result2= " + result2);
+
+        byte b = 100;
+        int result3 = -b;
+        int result4 = +b;
+        System.out.println("result3= " + result3);
+        System.out.println("result4= " + result4);
+
+    }
+}
+
