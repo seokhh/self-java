a44b662546227bb3babc9055ee4273f2486b62f1
seokhh <119908334+seokhh@users.noreply.github.com>
2023-01-23 20:35:28 +0900

    Add files via upload

---
+++ 혼공자 p.112 직접해보는 손코딩.java
@@ -0,0 +1,37 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+
+        int x = 10;
+        int y = 10;
+        int z;
+
+        System.out.println("--------------------");
+        x++;
+        ++x;
+        System.out.println("x= " + x); //x++: 10 + 1 = 11, ++X: 11+1 = 12
+
+        System.out.println("--------------------");
+        y--;
+        --y;
+        System.out.println("y= " + y); // y--: 10 - 1 = 9, --y: 9 - 1 = 8
+
+        System.out.println("--------------------");
+        z = x++;
+        System.out.println("z= " + z); // 12 = 13
+        System.out.println("x= " + x); // 13
+
+        System.out.println("--------------------");
+        z = ++x;
+        System.out.println("z= " + z); // 13 + 1 = 14
+        System.out.println("x= " + x); // 13 + 1 = 14
+
+        System.out.println("--------------------");
+        z = ++x + y++;
+        System.out.println("z= " + z); // 15 + 8 = 23
+        System.out.println("x= " + x); // 14 + 1 15
+        System.out.println("y= " + y); // 8 + 1 = 9
+    }
+}
+
