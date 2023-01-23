206aa43a33a815ec8a1659c77b38458a62b30fa5
seokhh <119908334+seokhh@users.noreply.github.com>
2023-01-23 20:41:21 +0900

    Add files via upload

---
+++ 혼공자 p.114 직접해보는 손코딩.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+
+        boolean play = true;
+        System.out.println(play);
+
+        play = !true;
+        System.out.println(play);
+
+        play = !play;
+        System.out.println(play);
+    }
+}
+
