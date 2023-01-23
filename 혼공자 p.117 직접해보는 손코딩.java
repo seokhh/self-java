77471a1a60e504479d7bd720278dff8d341aa907
seokhh <119908334+seokhh@users.noreply.github.com>
2023-01-23 20:47:16 +0900

    Add files via upload

---
+++ 혼공자 p.117 직접해보는 손코딩.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        char c1 = 'A' + 1;
+        char c2 = 'A';
+        int c3 = c2 + 1;
+
+        System.out.println("c1: " +c1);
+        System.out.println("c2: " +c2);
+        System.out.println("c3: " +c3);
+    }
+}
+
