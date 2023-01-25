98cd8332f298be8864e030dd541c79b5921aea57
seokhh <119908334+seokhh@users.noreply.github.com>
2023-01-25 15:06:31 +0900

    Add files via upload

---
+++ 혼공자 p.124 직접해보는 손코딩.java
@@ -0,0 +1,18 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        int result = 0;
+        result += 10;
+        System.out.println("result= " + result);
+        result -= 5;
+        System.out.println("result= " + result);
+        result += 10;
+        System.out.println("result= " + result);
+        result -= 12;
+        System.out.println("result= " + result);
+        result -= 3;
+        System.out.println("result= " + result);
+    }
+}
+
