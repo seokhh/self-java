25b9bd64adfc3ba6d62970b91b300fa3768f1690
seokhh <119908334+seokhh@users.noreply.github.com>
2023-01-25 15:11:17 +0900

    Add files via upload

---
+++ 혼공자 p.126 직접해보는 손코딩.java
@@ -0,0 +1,11 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+
+        int score = 85;
+        char grade = (score > 90) ? 'A' : ( (score > 80) ? 'B' : 'C');
+        System.out.println(score + "점은 " + grade + "등급입니다.");
+    }
+}
+
