d4bf2d31c62afddcdd8c20816ac074a71c158462
seokhh <119908334+seokhh@users.noreply.github.com>
2023-01-26 23:27:09 +0900

    Add files via upload

---
+++ 혼공자 p.150 직접해보는 손코딩.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        int b = sc.nextInt();
+        for(int a = 1; a<b; a++){
+            System.out.println(a);
+        }
+
+    }
+}
+
