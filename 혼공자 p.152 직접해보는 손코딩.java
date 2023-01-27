4ff7ae3a645db23bda5bf508bd6e403268e03bec
seokhh <119908334+seokhh@users.noreply.github.com>
2023-01-27 10:47:00 +0900

    Add files via upload

---
+++ 혼공자 p.152 직접해보는 손코딩.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+
+
+        int sum = 0;
+        int a =0;
+        for(a = 1; a<=100; a++){
+            sum += a;
+        }
+        System.out.println("1~" + (a -1) + " 까지의 합은: " + sum);
+    }
+}
+
