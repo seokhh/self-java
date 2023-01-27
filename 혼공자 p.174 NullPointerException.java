754e6d3debdcfe5afe175366b20d5e730b20dc4d
seokhh <119908334+seokhh@users.noreply.github.com>
2023-01-27 11:35:16 +0900

    Add files via upload

---
+++ 혼공자 p.174 NullPointerException.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+
+        String hobby = null;
+        /*System.out.println(hobby.length()); nullPointerException 발생*/
+
+        String name = "홍자바";
+        name = null; // 홍자바에서 값을 잃어서 쓰레기 객체가 됨.
+
+        
+    }
+}
+
