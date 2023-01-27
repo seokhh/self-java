760b4b5a20e25830e0b2f8e2066b76e700b92857
seokhh <119908334+seokhh@users.noreply.github.com>
2023-01-27 10:54:17 +0900

    Add files via upload

---
+++ 혼공자 p.154 직접해보는 손코딩.java
@@ -0,0 +1,12 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        int i = 1;
+        while (i<=10){
+            System.out.println(i);
+            i++;
+        }
+    }
+}
+
