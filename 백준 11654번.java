032bb1a3d28f80047eaafe04126f48d06f278c94
seokhh <119908334+seokhh@users.noreply.github.com>
2023-01-30 15:05:04 +0900

    Add files via upload

---
+++ 백준 11654번.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        String a = sc.nextLine();
+
+        int b = a.charAt(0);
+        System.out.println("입력한 알파벳의 아스키코드: " + b);
+    }
+}
