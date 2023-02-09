f80626a76322afd9e2c05175bbbdf369c01b2814
seokhh <119908334+seokhh@users.noreply.github.com>
2023-02-09 13:44:03 +0900

    Add files via upload

---
+++ 혼공자 p.295 직접해보는 손코딩/package1/A.java
@@ -0,0 +1,5 @@
+package sec06.exam03.package1;
+
+class A {
+// public을 없애면 default 접급 제한을 가지게 되어 같은 패키지 내에서만 사용가능
+}
---
+++ 혼공자 p.295 직접해보는 손코딩/package2/C.java
@@ -0,0 +1,6 @@
+package sec06.exam03.package2;
+
+public class C {
+    // A field;  A가 public이면 오류가 발생하지 않는다. 쓰기 위해선
+    // import sec06.exam06.package1.*;을 작성해야한다.
+}
