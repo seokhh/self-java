c05bfaa5a2d5a1b76af298cf0bdc4994298ab98a
seokhh <119908334+seokhh@users.noreply.github.com>
2023-03-11 18:48:40 +0900

    Add files via upload

---
+++ 혼공자 p.460 직접해보는 손코딩.java
@@ -0,0 +1,27 @@
+package selfjava.Chap10.sec02.exam04;
+
+public class CatchByExceptionKindExample {
+    public static void main(String[] args){
+
+        String data1 = null;
+        String data2 = null;
+
+        try {
+            data1 = args[0];
+            data2 = args[1];
+
+            int value1 = Integer.parseInt(data1);
+            int value2 = Integer.parseInt(data2);
+            int result = value1 + value2;
+            System.out.println(data1 + "+" + data2 + "=" + result);
+        } catch (NumberFormatException e){
+            System.out.println("숫자로 변환할 수 없습니다.");
+        } catch (Exception e){
+            System.out.println("실행 매개값의 수가 부족합니다.");
+        } finally {
+            System.out.println("다시 실행하세요.");
+        }
+
+        //configuration에 따라 값이 변할 수 있다.
+    }
+}
